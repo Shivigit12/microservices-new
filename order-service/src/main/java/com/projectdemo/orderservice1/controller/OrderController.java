815fb70bcc1afe1a4dc3aaf72dc6f269602ae8f2
welcome <shivam.singhs0807@gmail.com>
2023-02-14 13:08:42 +0530

    microservices initial commit

---
+++ order-service/src/main/java/com/projectdemo/orderservice1/controller/OrderController.java
@@ -0,0 +1,20 @@
+package com.projectdemo.orderservice1.controller;
+
+import com.projectdemo.orderservice1.dto.OrderRequest;
+import com.projectdemo.orderservice1.service.OrderService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/order")
+@RequiredArgsConstructor
+public class OrderController {
+    private final OrderService orderService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public String placeOrder(@RequestBody OrderRequest orderRequest){
+        orderService.placeOrder(orderRequest);
+        return "Order placed successfully!";
+    }
+}
